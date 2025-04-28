338519e54ccd8fc07b65c65a052ea27b0bdb97f6
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-29 02:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //your JS code here. If require
-for(let i=1;i<=20;i++){
-	let result=""
-	if(i%3==0 && i%5==0){
-		result="FizzBuzz"
-	}
-	else (i%3==0){
-		result="Fizz";
-	}
-	else if(i%5==0){
-		result ="Buzz"
-	}
+let output = "";
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += "FizzBuzz\n";
+  } else if (i % 3 === 0) {
+    output += "Fizz\n";
+  } else if (i % 5 === 0) {
+    output += "Buzz\n";
+  } else {
+    output += i + "\n";
+  }
+}
+
+alert(output);
 	
-}	
