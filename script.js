67ae473003a42171e9b6ea44aeb4314cbb88bdd5
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-29 02:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+//your JS code here. If require
+for(let i=1;i<=20;i++){
+	let result=""
+	if(i%3==0 && i%5==0){
+		result="FizzBuzz"
+	}
+	else (i%3==0){
+		result="Fizz";
+	}
+	else if(i%5==0){
+		result ="Buzz"
+	}
+	
+}	
